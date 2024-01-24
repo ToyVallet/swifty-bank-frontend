d367fcd55c11630a1fb705ea5b2b757311c55300
이호연 <ho991217@kakao.com>
2024-01-24 18:28:30 +0900

    feat: 인풋 컴포넌트 작성

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -1,13 +1,50 @@
 'user client';
 
-interface InputProps {
-  type: string;
+import {
+  Children,
+  cloneElement,
+  ForwardedRef,
+  forwardRef,
+  HTMLAttributes,
+  InputHTMLAttributes,
+  ReactElement,
+  ReactNode,
+} from 'react';
+
+interface InputProps extends HTMLAttributes<HTMLDivElement> {
+  label?: ReactNode;
+  children: ReactElement;
+  bottomText?: string;
 }
 
-export default function Input() {
+export function Input({ label, children, bottomText, ...props }: InputProps) {
+  const child = Children.only(children);
+  const generatedId = useId('input');
+  const id = child.props.id ?? generatedId;
+  const isError: boolean = child.props.error ?? false;
+
   return (
-    <div>
-      <h1>Input Component</h1>
+    <div {...props}>
+      <label htmlFor={id}>{label}</label>
+      {cloneElement(child, {
+        id,
+        ...child.props,
+      })}
+      {bottomText != null ? <p>{bottomText}</p> : null}
     </div>
   );
 }
+
+interface TextFieldProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> {
+  error?: boolean;
+}
+
+Input.TextField = forwardRef(
+  (
+    { error, ...props }: TextFieldProps,
+    ref: ForwardedRef<HTMLInputElement>
+  ) => {
+    return <input ref={ref} {...props} />;
+  }
+);
