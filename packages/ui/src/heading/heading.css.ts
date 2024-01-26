0ddd096469ad80c88966bf3a456981f7a339fe21
ddaeunbb <ddaeunbb@gmail.com>
2024-01-26 20:24:51 +0900

    style: 헤딩 스타일 수정 - font-weight 속성을 추가해주었습니다.

--- packages/ui/src/heading/heading.css.ts
+++ packages/ui/src/heading/heading.css.ts
@@ -3,16 +3,19 @@ import { style } from '@vanilla-extract/css';
 const head1 = style({
   color: 'black',
   fontSize: '28px',
+  fontWeight: '700',
 });
 
 const head2 = style({
   color: 'black',
   fontSize: '24px',
+  fontWeight: '700',
 });
 
 const head3 = style({
   color: 'black',
   fontSize: '20px',
+  fontWeight: '700',
 });
 
 const styles = {
