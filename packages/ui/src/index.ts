5a710ac7aa3fafbe66ccbd645e122e7e56d5ba99
ddaeunbb <ddaeunbb@gmail.com>
2024-01-29 18:10:04 +0900

    fix: lint, prettier 충돌 해결

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,4 @@
 // 유저 인터렉션 컴포넌트
 export { default as Button } from './button';
 export { default as Input } from './input';
-export { default as Heading } from './heading'
+export { default as Heading } from './heading';
