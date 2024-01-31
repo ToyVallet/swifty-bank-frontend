7b026abe26440877250cd8a1a023fffa90bd840c
이호연 <ho991217@kakao.com>
2024-01-31 14:04:30 +0900

    chore: bank의 컴포넌트들 삭제
    충돌 방지를 위함

--- apps/swifty-bank/app/page.tsx
+++ apps/swifty-bank/app/page.tsx
@@ -1,27 +1,7 @@
 "use client";
 
-import { container } from './page.css';
-import { Input, Heading } from '@swifty/ui';
+import { container } from "./page.css";
 
 export default function Page() {
-  const [selectedOption, setSelectedOption] = useState("");
-  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    setSelectedOption(() => e.target.value);
-  };
-
-  const handleClick = () => {
-    console.log(selectedOption);
-  };
-
-  return (
-    <main className={container}>
-      <Input label="성명">
-        <Input.Text placeholder="이름을 입력하세요." />
-      </Input>
-
-      <Heading>회원정보를 입력해주세요.</Heading> 
-      <Heading type='h2'>회원정보를 입력해주세요.</Heading> 
-      <Heading type='h3'>회원정보를 입력해주세요.</Heading> 
-    </main>
-  );
+  return <main className={container}></main>;
 }
