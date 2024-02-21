e682661e38bae0e536ad5e31ef31d2d6c1d271b4
JUNSEOK <kd02109@gmail.com>
2024-02-21 17:58:38 +0900

    fix : on- handle- 수정

--- packages/ui/src/checkbox/index.tsx
+++ packages/ui/src/checkbox/index.tsx
@@ -3,7 +3,7 @@
 import RightIcon from "../icon/RightIcon";
 import CheckIcon from "../icon/CheckIcon";
 import styles from "./checkbox.css";
-import { HTMLAttributes, useState, useCallback } from "react";
+import { HTMLAttributes, useState } from "react";
 import { createPortal } from "react-dom";
 import Detail from "./detail";
 import { CheckBoxProp } from "@swifty/hooks";
@@ -21,45 +21,41 @@ const CheckBox = ({
 }: Prop) => {
   const [isOpened, setIsOpened] = useState(false);
 
-  const handleOpen = useCallback(() => {
+  const onOpen = () => {
     setIsOpened(true);
-  }, []);
+  };
 
-  const handleClose = useCallback(() => {
+  const onClose = () => {
     setIsOpened(false);
-  }, []);
+  };
 
   return (
-    <>
-      <section className={styles.container}>
-        <div className={styles.leftSection} onChange={onChange}>
-          <input type="checkbox" id={id} className={styles.inputCheck} />
-          <CheckIcon
-            isCheck={isCheck}
-            className={styles.svg}
-            onChange={onChange}
-          />
-          <label htmlFor={id} className={styles.label}>
-            {labelContent}
-          </label>
-        </div>
-        <div className={styles.rightSection}>
-          {isDetail && (
-            <RightIcon onClick={handleOpen} className={styles.svg} />
-          )}
-        </div>
-        {isDetail &&
-          isOpened &&
-          createPortal(
-            <Detail
-              onClick={handleClose}
-              detailInfoType={detailInfoType!}
-              isOpened={isOpened}
-            />,
-            document.body,
-          )}
-      </section>
-    </>
+    <section className={styles.container}>
+      <div className={styles.leftSection} onChange={onChange}>
+        <input type="checkbox" id={id} className={styles.inputCheck} />
+        <CheckIcon
+          isCheck={isCheck}
+          className={styles.svg}
+          onChange={onChange}
+        />
+        <label htmlFor={id} className={styles.label}>
+          {labelContent}
+        </label>
+      </div>
+      <div className={styles.rightSection}>
+        {isDetail && <RightIcon onClick={onOpen} className={styles.svg} />}
+      </div>
+      {isDetail &&
+        isOpened &&
+        createPortal(
+          <Detail
+            onClick={onClose}
+            detailInfoType={detailInfoType!}
+            isOpened={isOpened}
+          />,
+          document.body,
+        )}
+    </section>
   );
 };
 
