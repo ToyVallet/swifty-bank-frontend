449de6de6b4e8206a542efc40ccc432c7d9869c2
JUNSEOK <kd02109@gmail.com>
2024-02-26 19:20:29 +0900

    feat: button disabled 속성 설정

--- apps/swifty-bank/app/signup/_component/check-form/CheckForm.tsx
+++ apps/swifty-bank/app/signup/_component/check-form/CheckForm.tsx
@@ -18,10 +18,11 @@ interface FormData {
 }
 
 export default function CheckForm({ onClose }: Prop) {
-  const { register, handleSubmit, setValue } = useForm<FormData>({});
+  const { register, handleSubmit, setValue, formState } = useForm<FormData>({});
 
   const onSubmit: SubmitHandler<FormData> = (data) => {
     console.log(data);
+    console.log(formState);
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={styles.container}>
@@ -29,28 +30,31 @@ export default function CheckForm({ onClose }: Prop) {
         <CheckDetail
           detailInfoType="personalInfo"
           labelContent={"[필수] 개인정보 처리방침 동의"}
-          inputref={register("personalInfo")}
+          inputref={register("personalInfo", { required: true })}
           setValue={setValue}
           uniqueResiter="personalInfo"
         />
         <CheckDetail
           detailInfoType="service"
           labelContent={"[필수] 서비스 이용약관 동의"}
-          inputref={register("service")}
+          inputref={register("service", { required: true })}
           setValue={setValue}
           uniqueResiter="service"
         />
         <CheckDetail
           detailInfoType="promotion"
           labelContent={"[선택] 프로모션 등 안내 동의"}
-          inputref={register("promotion")}
+          inputref={register("promotion", { required: false })}
           setValue={setValue}
           uniqueResiter="promotion"
         />
       </div>
 
       <div className={styles.buttonSection}>
-        <Button type="submit" variant={"active"}>
+        <Button
+          type="submit"
+          variant={!formState.isValid ? "disabled" : "active"}
+        >
           동의하기
         </Button>
         <Button onClick={onClose} className={styles.closeButton}>
