16982698c9823f57295223cec6f08f78ea7d9939
doctork4 <doctor.k1994@gmail.com>
2024-03-11 15:03:23 +0900

    feat: API handler 함수 추가

---
+++ apps/swifty-bank/app/_api/util.ts
@@ -0,0 +1,116 @@
+const commonHeaders = {
+  "Content-Type": "application/json",
+};
+
+async function getWithToken(
+  url: string,
+  token: string,
+  headerOptions: Record<string, unknown> = {},
+  errorMessage: string = "",
+) {
+  try {
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        credentials: "include",
+        ...commonHeaders,
+        ...headerOptions,
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error((await res.json()).message);
+    }
+
+    return res.json();
+  } catch (error) {
+    throw new Error(errorMessage);
+  }
+}
+
+async function postWithToken<R>(
+  url: string,
+  data: Record<string, unknown>,
+  token: string,
+  headerOptions: Record<string, unknown> = {},
+  errorMessage: string = "",
+): Promise<R> {
+  try {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        credentials: "include",
+        ...commonHeaders,
+        ...headerOptions,
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!res.ok) {
+      throw new Error((await res.json()).message);
+    }
+
+    return res.json();
+  } catch (error) {
+    throw new Error(errorMessage);
+  }
+}
+
+async function postWithoutToken<R>(
+  url: string,
+  data: Record<string, unknown>,
+  headerOptions: Record<string, unknown> = {},
+  errorMessage: string = "",
+): Promise<R> {
+  try {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        ...commonHeaders,
+        ...headerOptions,
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!res.ok) {
+      throw new Error(res.statusText);
+    }
+
+    return res.json();
+  } catch (error) {
+    throw new Error(errorMessage);
+  }
+}
+
+async function patchWithToken<R>(
+  url: string,
+  data: Record<string, unknown>,
+  token: string,
+  headerOptions: Record<string, unknown> = {},
+  errorMessage: string = "",
+): Promise<R> {
+  try {
+    const res = await fetch(url, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        credentials: "include",
+        ...commonHeaders,
+        ...headerOptions,
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!res.ok) {
+      throw new Error((await res.json()).message);
+    }
+
+    return res.json();
+  } catch (error) {
+    throw new Error(errorMessage);
+  }
+}
+
+export { getWithToken, postWithToken, postWithoutToken, patchWithToken };
