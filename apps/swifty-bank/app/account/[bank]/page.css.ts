a35ea92f20dc35049db69e0cf6123aec270edbaa
JUNSEOK <kd02109@gmail.com>
2024-03-14 20:13:40 +0900

    feat: 계좌 환 페이지 적용

---
+++ apps/swifty-bank/app/account/[bank]/page.css.ts
@@ -0,0 +1,24 @@
+import { vars } from "@swifty/ui/styles/vars.css";
+import { style } from "@vanilla-extract/css";
+
+const imageSection = style({
+  marginTop: vars.space[8],
+  display: "grid",
+  gap: vars.space[4],
+  gridTemplateColumns: "repeat(2, 1fr)",
+  gridTemplateRows: "repeat(2, 1fr)",
+});
+
+const imageContainer = style({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  boxShadow: "0px 0px 6px rgba(0, 0, 0, 0.15)",
+  borderRadius: 25,
+  width: 162,
+  height: 192,
+});
+const styles = { imageSection, imageContainer };
+
+export default styles;
--- apps/swifty-bank/app/account/[bank]/page.tsx
+++ apps/swifty-bank/app/account/[bank]/page.tsx
@@ -1,3 +1,35 @@
-export default function BankPage() {
-  return <div></div>;
+import CancelButton from "@/_component/CancelButton";
+import Template from "@/_component/Template";
+import Image from "next/image";
+import styles from "@/account/[bank]/page.css";
+
+type Props = {
+  params: { bank: string };
+};
+export default function BankPage({ params: { bank } }: Props) {
+  return (
+    <Template>
+      <CancelButton />
+      <Template.Header
+        main="Swifty는 이러한 환을 지원해요"
+        sub={
+          "swifty는 기본적으로 아래의 네 가지 환을 지원해요.\n 이외의 다양한 환을 관리해보세요."
+        }
+      />
+      <section className={styles.imageSection}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/img/account/won.png"
+            alt="won"
+            width={135}
+            height={105}
+          />
+          <span>한국 원화</span>
+        </div>
+        <div>일본 엔화</div>
+        <div>중국 위완화</div>
+        <div>미국 달러</div>
+      </section>
+    </Template>
+  );
 }
