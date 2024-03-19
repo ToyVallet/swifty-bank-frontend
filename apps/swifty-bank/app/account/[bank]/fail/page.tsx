870d09a43c398d7b4976ff323fabd9ff80243107
JUNSEOK <kd02109@gmail.com>
2024-03-19 17:55:02 +0900

    feat: 로딩 페이지 개설 실페 페이지 구현

--- apps/swifty-bank/app/account/[bank]/fail/page.tsx
+++ apps/swifty-bank/app/account/[bank]/fail/page.tsx
@@ -1,3 +1,21 @@
+import CancelButton from "@/_component/CancelButton";
+import Template from "@/_component/Template";
+import BackButton from "@/account/[bank]/fail/_component/BackButton";
+import Image from "next/image";
+
 export default function AccountFailPage() {
-  return;
+  return (
+    <Template>
+      <CancelButton href="/account" />
+      <Template.Header
+        main={"계좌 개설에\n오류가 발생했습니다."}
+        sub="계좌 개설 과정을 다시 한번 확인해주세요"
+        type="center"
+      />
+      <Template.MainIcon type="fail">
+        <Image src={"/img/error.png"} alt="error" width={180} height={180} />
+      </Template.MainIcon>
+      <BackButton />
+    </Template>
+  );
 }
--- apps/swifty-bank/app/account/[bank]/loading/page.tsx
+++ apps/swifty-bank/app/account/[bank]/loading/page.tsx
@@ -1,3 +1,22 @@
-export default function AccountLoadingPage() {
-  return;
+import Template from "@/_component/Template";
+import Image from "next/image";
+
+export default async function AccountLoadingPage() {
+  return (
+    <Template>
+      <Template.Header
+        type="center"
+        main={"계좌 개설이\n 진행되고 있습니다"}
+        sub="조금만 기다려 주세요"
+      />
+      <Template.MainIcon>
+        <Image
+          src={"/img/loading.png"}
+          alt="loading"
+          width={250}
+          height={280}
+        />
+      </Template.MainIcon>
+    </Template>
+  );
 }
